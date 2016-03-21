0c9f78c5d2398babada8bcc012e7c16410df85ca
jai-bridg <jai@bridg.com>
2016-03-21 16:40:14 -0700

    finish naive bayes script
    finish naive bayes script

--- naive_bayes_benchmark.R
+++ naive_bayes_benchmark.R
@@ -1,4 +1,4 @@
-# This script creates and evaluates an adaboost model in R for the 'iris' data.
+# This script creates and evaluates an Naive Bayes model in R for the 'iris' data.
 # This iteration is only to get the model up and running, so there is no feature engineering and parameter tuning.
 
 # Load packages.
@@ -6,27 +6,11 @@ library(data.table)
 library(e1071)
 
 # Import data.
-train = data.table(read.csv('../input/train.csv',
+train = data.table(read.csv('train.csv',
                             header = T))
-test = data.table(read.csv('../input/test.csv', 
+test = data.table(read.csv('test.csv', 
                            header = T))
 
-#####
-# PRE-PROCESSING AND FEATURE ENGINEERING.
-  
-  # Rename columns of testing and training data set.
-  setnames(train, names(train), c('date', 'category_predict', 'description_ignore', 'day_of_week', 'pd_district', 'resolution', 'address', 'x', 'y'))
-  setnames(test, names(test), c('id', 'date', 'day_of_week', 'pd_district', 'address', 'x', 'y'))
-  
-  # Get hour of each crime.
-  train$hour = as.numeric(substr(train$date, 12, 13))
-  test$hour = as.numeric(substr(test$date, 12, 13))
-  
-  # Create data subset to train model on (training on full data set is too time-intensive).
-  # 'subset' contains at least 1 example of every crime category.
-  subset = train[c(1:30000, 30024, 33954, 37298, 41097, 41980, 44479, 48707, 48837, 49306, 53715, 93717, 102637, 102645, 102678, 102919, 103517, 103712, 107734, 148476, 148476, 
-                   192191, 205046, 252094, 279792, 316491, 317527, 332821, 337881)]
-  
 #####
 # CREATE MODEL AND PREDICTIONS.
   
@@ -34,41 +18,38 @@ test = data.table(read.csv('../input/test.csv',
   set.seed(1)
   
   # Define model.
-  model = category_predict ~ day_of_week + pd_district + x + y + hour
+  model = Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
   
   # Create model and generate predictions for training set.
   nb = naiveBayes(model, 
-                  data = subset)
+                  data = train)
   
   # Generate predictions for training and test data.
-  # For the training data, I only want to compute accuracy.
-  # For the test data, I need to put predictions in a specific format for submission, as specified by Kaggle.com.
-  subset$pred = data.table(predict(nb, 
-                                   newdata = subset, 
+  train$pred = data.table(predict(nb, 
+                                   newdata = train, 
                                    type = 'class'))
-  test_pred = data.table(predict(nb, 
+  test$pred = data.table(predict(nb, 
                                  newdata = test, 
-                                 type = 'raw'))
+                                 type = 'class'))
 
 #####
 # CHECK TRAINING SET ACCURACY.
   
   # View training accuracy.
   print('Training Accuracy:')
-  table(subset$category_predict == subset$pred)
-  prop.table(table(subset$category_predict == subset$pred))
+  table(train$Species == train$pred)
+  prop.table(table(train$Species == train$pred))
 
 #####
 # CROSS VALIDATION.
     
   # Create 'tune.control' object.
-  # Certain crime categories don't have many occurences in 'subset' so I keep the number of cross validation partitions low.
   tune_control = tune.control(cross = 2)
   
   # Execute cross validation.
   t = tune(method = 'naiveBayes', 
            model, 
-           data = subset, 
+           data = train, 
            size = 2, 
            tunecontrol = tune_control)
   
@@ -77,14 +58,9 @@ test = data.table(read.csv('../input/test.csv',
   print(t)
 
 #####
-# EXPORT TEST SET PREDICTIONS.
+# CHECK TEST SET ACCURACY.
 
-  # Predictions must be formatted as specified on Kaggle.com.
-  # This is done for test data only.
-  
-    # Add 'test$id' to 'test_pred'.
-    test_pred$id = test$id
-  
-  # Create csv file of test predictions.
-  # This is commented out for now, since I don't actually want to create a csv.
-  # write.csv(test_pred, 'test_pred_naive_bayes_benchmark.csv', row.names = F)
+  # View test set accuracy.
+  print('Test Set Accuracy:')
+  table(test$Species == test$pred)
+  prop.table(table(test$Species == test$pred))
