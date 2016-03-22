4557c0a99a7bb0832270ae1eb2e47a5c443f22af
jai-bridg <jai@bridg.com>
2016-03-21 17:13:22 -0700

    add support vector machine script
    add support vector machine script

---
+++ support_vector_machine_benchmark.R
@@ -0,0 +1,75 @@
+# This script creates and evaluates a support vector machine model in R for the 'iris' data.
+# This iteration is only to get the model up and running, so there is no feature engineering and parameter tuning.
+
+# Load packages.
+library(data.table)
+library(e1071)
+
+# Import data.
+train = data.table(read.csv('train.csv',
+                            header = T))
+test = data.table(read.csv('test.csv', 
+                           header = T))
+
+#####
+# CREATE MODEL AND PREDICTIONS.
+
+  # Set seed to ensure reproducibility.
+  set.seed(1)
+
+  # Define model.
+  model = Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
+  
+  # Create SVM.
+  svm = svm(model, 
+            data = train, 
+            kernel = 'linear', 
+            probability = T,
+            cost = 0.01, 
+            scale = T)
+
+  # View indices of observations that are support vectors.
+  svm$index
+  
+  # View length of support vector index vector.
+  print(length(svm$index))
+  
+  # Generate predictions for training and test data.
+  train$pred = data.table(predict(svm,
+                               newdata = train,
+                               decision.values = F))
+  test$pred = predict(svm, 
+                      newdata = test, 
+                      decision.values = T)
+
+#####
+# CHECK TRAINING SET ACCURACY.
+
+  # View training accuracy.
+  print('Training Accuracy:')
+  table(train$Species == train$pred)
+  prop.table(table(train$Species == train$pred))
+    
+#####
+# CROSS VALIDATION.
+# Estimate test set error using cross validation.
+    
+    # Create 'tune.control' object.
+    tune_control = tune.control(cross = 2)
+    
+    # Execute cross validation.
+    t = tune('svm', 
+             model, 
+             data = train, 
+             tunecontrol = tune_control)
+    
+    # View estimated test set error computed by cross validation.
+    print(t)
+
+#####
+# TEST SET ACCURACY.
+
+  # View test set accuracy.
+  print('Test Set Accuracy:')
+  table(test$Species == test$pred)
+  prop.table(table(test$Species == test$pred))
